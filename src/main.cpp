1dd39438802a90404a6b9146edc025008e8992d8
Florian Taute <tautellini@outlook.de>
2023-09-09 20:36:06 +0200

    update to fix grav jump bug

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include "sfse_common/Relocation.h"
 #include "sfse_common/SafeWrite.h"
 
-RelocAddr <uintptr_t*> boostCheck = 0x24EF80B;
+RelocAddr <uintptr_t*> boostCheckWithGravJump = 0x24EF80B;
+RelocAddr <uintptr_t*> boostCalculation = 0x24EEFE5;
 
 extern "C" {
 	SFSEPluginVersionData SFSEPlugin_Version = {
@@ -21,9 +22,10 @@ extern "C" {
 
 	void SFSEPlugin_Load(const SFSEInterface* sfse)
 	{
-		// overrides the boost cost mov operation at 0x24EF80B + 5 bytes with null operations
-		// this way using the jetpack doesnt reduce the boost meter
-		BYTE Nop[] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
-		safeWriteBuf(boostCheck.getUIntPtr(), &Nop, sizeof(Nop));
+		//BYTE MovZero[] = { 0x42, 0xC7, 0x04, 0xA6, 0x00, 0x00, 0x00, 0x00, 0x90, 0x90 }; // will also make grav jump instant
+		//safeWriteBuf(boostCheckWithGravJump.getUIntPtr(), &MovZero, sizeof(MovZero)); // will also make grav jump instant
+
+		BYTE Nop[] = { 0x90, 0x90, 0x90, 0x90 };
+		safeWriteBuf(boostCalculation.getUIntPtr(), &Nop, sizeof(Nop));
 	}
 };
